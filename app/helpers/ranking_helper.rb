fa4a774cf59afec87cfd99b35f01a2a3cf4472f2
Leo Tanguy <leo0703@hotmail.fr>
2021-11-29 13:03:00 +0100

    Finished the update of ranking

---
+++ app/helpers/ranking_helper.rb
@@ -0,0 +1,20 @@
+module RankingHelper
+  def compute_last_games_results(user)
+    games = user.games.where("date < ?", Time.now).order("date DESC").first(5)
+
+    content_tag :div do
+      games.each do |game|
+        user_bet = game.bets.find_by(user: user)
+        other_user_bet = game.bets.where.not(user: user).first
+
+        if user_bet.end_result > other_user_bet.end_result
+          return content_tag(:div, "", class: "green-bullet bullets")
+        elsif user_bet.end_result == other_user_bet.end_result
+          return content_tag(:div, "", class: "gray-bullet bullets")
+        else
+          return content_tag(:div, "", class: "red-bullet bullets")
+        end
+      end
+    end
+  end
+end
