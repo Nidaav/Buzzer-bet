fcb11c12ec14c650f3f37058b7e0713a1e1eef96
Leo Tanguy <leo0703@hotmail.fr>
2021-11-30 15:28:46 +0100

    Creating league toussa tu conné

--- app/helpers/ranking_helper.rb
+++ app/helpers/ranking_helper.rb
@@ -1,20 +1,21 @@
 module RankingHelper
   def compute_last_games_results(user)
-    games = user.games.where("date < ?", Time.now).order("date DESC").first(5)
+    games = user.games.where("date < ?", Date.today).order("date DESC").first(5)
 
-    content_tag :div do
-      games.each do |game|
-        user_bet = game.bets.find_by(user: user)
-        other_user_bet = game.bets.where.not(user: user).first
+    @content = content_tag :div
+    games.each do |game|
+      user_bet = game.bets.find_by(user: user)
+      other_user_bet = game.bets.where.not(user: user).first
 
-        if user_bet.end_result > other_user_bet.end_result
-          return content_tag(:div, "", class: "green-bullet bullets")
-        elsif user_bet.end_result == other_user_bet.end_result
-          return content_tag(:div, "", class: "gray-bullet bullets")
-        else
-          return content_tag(:div, "", class: "red-bullet bullets")
-        end
+      if user_bet.end_result > other_user_bet.end_result
+        @content << content_tag(:div, "", class: "green-bullet bullets")
+      elsif user_bet.end_result == other_user_bet.end_result
+        @content << content_tag(:div, "", class: "gray-bullet bullets")
+      else
+        @content << content_tag(:div, "", class: "red-bullet bullets")
       end
     end
+
+    return @content
   end
 end
