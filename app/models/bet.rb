0f09e035cdfb80553edfa11172a01b9bf4d504db
Leo Tanguy <leo0703@hotmail.fr>
2021-12-01 10:07:24 +0100

    finished show

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -8,8 +8,15 @@ class Bet < ApplicationRecord
     self.end_result += 5 if top_rebounder == game.top_rebounder
     self.end_result += 5 if top_passer == game.top_passer
     self.end_result += 5 if winner == game.winner
+
+    self.end_result += 5 if (game.gap_points - 10..game.gap_points + 10).include? gap_points
+    self.end_result += 10 if (game.gap_points - 5..game.gap_points + 5).include? gap_points
     self.end_result += 15 if gap_points == game.gap_points
-    self.end_result += 20 if total_points == game.total_points
+
+    self.end_result += 5 if (game.total_points - 30..game.total_points + 30).include? total_points
+    self.end_result += 10 if (game.total_points - 20..game.total_points + 20).include? total_points
+    self.end_result += 15 if (game.total_points - 10..game.total_points + 10).include? total_points
+    self.end_result += 50 if total_points == game.total_points
 
     return self.end_result
   end
