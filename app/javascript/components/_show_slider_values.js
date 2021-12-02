cb5a7a3aef0e2808846e2253d814eeb608c2a06c
Valentin Busnel <valentinbusnel27@gmail.com>
2021-12-02 14:07:59 +0100

    vfd

--- app/javascript/components/_show_slider_values.js
+++ app/javascript/components/_show_slider_values.js
@@ -1,19 +1,20 @@
 const showSliderValues = () => {
-  const betPage = document.querySelector(".container-league-home")
+  const betPage = document.querySelector(".container-edit")
   if (betPage) {
     const inputTotalPoints = document.getElementById('bet_total_points');
     const countTotalPoints = document.getElementById('count-total-points');
     const inputGapPoints = document.getElementById('bet_gap_points')
     const countGapPoints = document.getElementById('count-gap-points');
+    countGapPoints.innerText = inputGapPoints.value;
+    countTotalPoints.innerText = inputTotalPoints.value;
 
-
-    inputTotalPoints.addEventListener('change', (e) => {
+    inputTotalPoints.addEventListener('input', (e) => {
       countTotalPoints.style.opacity = 1;
       countTotalPoints.innerText = inputTotalPoints.value;
       countTotalPoints.style.left = `50%`;
     })
 
-    inputGapPoints.addEventListener('change', (e) => {
+    inputGapPoints.addEventListener('input', (e) => {
       countGapPoints.style.opacity = 1;
       countGapPoints.innerText = inputGapPoints.value;
       countGapPoints.style.left = `${inputGapPoints.value}%`;
