57dd3726abeaa2fa94606a16caa2896cdc0d4701
Valentin Busnel <valentinbusnel27@gmail.com>
2021-12-02 12:11:53 +0100

    vfd

--- app/javascript/components/_scroll_bar.js
+++ app/javascript/components/_scroll_bar.js
@@ -1,11 +1,10 @@
 const scrollBar = () => {
   const progressBar = document.querySelector(".scrollbar");
 
-  let totalHeight = document.documentElement.scrollHeight -  document.documentElement.clientHeight
-
-  window.addEventListener("mousewheel", (e) => {
-    //console.log(e.y)
-    let progress = (document.documentElement.scrollTop / totalHeight) * 100
+  const main = document.querySelector('.main')
+  let totalHeight = main.scrollHeight - main.clientHeight;
+  main.addEventListener("scroll", (e) => {
+    let progress = (main.scrollTop / totalHeight) * 100;
     progressBar.style.height = `${progress}%`
     progressBar.style.opacity = `${progress}%`
   })
