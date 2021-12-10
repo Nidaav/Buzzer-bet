c26ee1c0ea1055e997ca8612bb0a81789f4554e5
Leo Tanguy <leo0703@hotmail.fr>
2021-12-10 15:54:31 +0100

    started job to auto refresh

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,7 +1,12 @@
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
-
+  queue_as :default
+  def refresh_info
+    puts "I'm starting the fake job"
+    sleep 3
+    puts "OK I'm done now"
+  end
+end
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
-end
